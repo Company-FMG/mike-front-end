bc7336b605e8b15b303b10829020c10314ea264c
RafaFigueiroa <rafatinsfig@gmail.com>
2024-06-12 16:24:35 -0300

    fix: minimun changes

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -8,22 +8,34 @@ export default function Home() {
     return (
         <AuthenticatedLayout>
             <div className="grid grid-cols-2 justify-items-center px-40 gap-y-20">
-                <div className="overflow-auto border-4 rounded-lg border-gray-200 px-10 py-8 space-y-8">
+                <div className="border-4 rounded-lg border-gray-200 px-10 pt-6 space-y-8">
                     <div className="flex justify-between items-center">
                         <h3 className="font-poppins font-bold text-bluemike text-xl">Novas Denúncias</h3>
                         <button onClick={() => navigate('/newcomplaint')} className="bg-bluemike text-white h-10 w-10 text-5xl rounded-full flex items-center pb-3 justify-center">+</button>
                     </div>
-                    <ComplaintCard status="nova" tipo="#121 - VBG" endereco="R. Patrícia de Fernandes - Espinheiro"/>
-                    <ComplaintCard status="nova" tipo="#121 - VBG" endereco="R. Patrícia de Fernandes - Espinheiro"/>
+                    <div className="overflow-auto max-h-[460px] space-y-7">
+                        <ComplaintCard status="nova" tipo="#121 - VBG" endereco="R. Patrícia de Fernandes - Espinheiro"/>
+                        <ComplaintCard status="nova" tipo="#121 - VBG" endereco="R. Patrícia de Fernandes - Espinheiro"/>
+                        <ComplaintCard status="nova" tipo="#121 - VBG" endereco="R. Patrícia de Fernandes - Espinheiro"/>
+                        <ComplaintCard status="nova" tipo="#121 - VBG" endereco="R. Patrícia de Fernandes - Espinheiro"/>
+                    </div>
                 </div>
-                <div className="overflow-auto border-4 rounded-lg border-gray-200 px-10 py-8 space-y-8">
+                <div className="border-4 rounded-lg border-gray-200 px-10 pt-6 space-y-8">
                     <h3 className="font-poppins font-bold text-bluemike text-xl">Ocorrências em andamento</h3>
-                    <ComplaintCard status="em andamento" tipo="#121 - VBG" endereco="R. Patrícia de Fernandes - Espinheiro"/>
-                    <ComplaintCard status="em andamento" tipo="#121 - VBG" endereco="R. Patrícia de Fernandes - Espinheiro"/>
+                    <div className="overflow-auto max-h-[460px] space-y-7">
+                        <ComplaintCard status="em andamento" tipo="#121 - VBG" endereco="R. Patrícia de Fernandes - Espinheiro"/>
+                        <ComplaintCard status="em andamento" tipo="#121 - VBG" endereco="R. Patrícia de Fernandes - Espinheiro"/>
+                        <ComplaintCard status="em andamento" tipo="#121 - VBG" endereco="R. Patrícia de Fernandes - Espinheiro"/>
+                    </div> 
                 </div>
-                <div className="col-span-2 overflow-auto border-4 rounded-lg border-gray-200 px-10 py-8 space-y-8">
+                <div className="col-span-2 overflow-auto border-4 rounded-lg border-gray-200 px-10 pt-8 space-y-8">
                     <h3 className="font-poppins font-bold text-bluemike text-xl">Ocorrências finalizadas</h3>
-                    <div className="grid grid-cols-2 space-x-24">
+                    <div className="grid grid-cols-2 gap-x-24 overflow-auto max-h-[460px] gap-y-7">
+                        <ComplaintCard status="finalizada" tipo="#121 - VBG" endereco="R. Patrícia de Fernandes - Espinheiro"/>
+                        <ComplaintCard status="finalizada" tipo="#121 - VBG" endereco="R. Patrícia de Fernandes - Espinheiro"/>
+                        <ComplaintCard status="finalizada" tipo="#121 - VBG" endereco="R. Patrícia de Fernandes - Espinheiro"/>
+                        <ComplaintCard status="finalizada" tipo="#121 - VBG" endereco="R. Patrícia de Fernandes - Espinheiro"/>
+                        <ComplaintCard status="finalizada" tipo="#121 - VBG" endereco="R. Patrícia de Fernandes - Espinheiro"/>
                         <ComplaintCard status="finalizada" tipo="#121 - VBG" endereco="R. Patrícia de Fernandes - Espinheiro"/>
                         <ComplaintCard status="finalizada" tipo="#121 - VBG" endereco="R. Patrícia de Fernandes - Espinheiro"/>
                     </div>
